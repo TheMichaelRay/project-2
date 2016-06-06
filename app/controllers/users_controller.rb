fb7d80842a0e68867cd75f96d3179094eeb91fd1
Michael Ray <raymichael23@gmail.com>
2016-06-06 16:14:13 -0700

    added user signup page that adds users to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,17 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -28,6 +36,6 @@ private
   end
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:email, :first_name, :last_name, :username, :password, :password_confirmation)
   end
 end
