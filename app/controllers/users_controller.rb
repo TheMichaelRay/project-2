00c13290ba441e620e7efbe5de200d07fb45d7d6
Michael Ray <raymichael23@gmail.com>
2016-06-07 11:14:14 -0700

    added paperclip gem to allow for image uploads, added profile images for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
-  before_action :authorize
+  # before_action :authorize
 
   def index
   end
@@ -37,6 +37,6 @@ private
   end
 
   def user_params
-    params.require(:user).permit(:email, :first_name, :last_name, :username, :password, :password_confirmation)
+    params.require(:user).permit(:email, :first_name, :last_name, :username, :password, :password_confirmation, :avatar)
   end
 end
