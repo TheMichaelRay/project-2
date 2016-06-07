ae271252b57b3565005dd5722031b5999b3daeb6
Michael Ray <raymichael23@gmail.com>
2016-06-07 11:41:44 -0700

    users can now post shoutouts. working on making shoutouts belong to their users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,22 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
   end
 
   def show
@@ -18,5 +29,14 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
-  # before_action :authorize
+  before_action :authorize, only: [:edit, :update, :destroy]
 
   def index
   end
