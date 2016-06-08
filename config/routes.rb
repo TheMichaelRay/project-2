91ca42558c5b264c65de67dc63c0d2451f5ca1cc
Michael Ray <raymichael23@gmail.com>
2016-06-07 21:57:29 -0700

    added comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+
+  get 'comments/new'
+
+  get 'comments/create'
+
+  get 'comments/update'
+
+  get 'comments/show'
+
+  get 'comments/edit'
+
+  get 'comments/destroy'
+
   #
   # get 'sessions/new'
   #
