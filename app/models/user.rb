98bca705922f98c9a249e6da628745f2fe413892
Michael Ray <raymichael23@gmail.com>
2016-06-08 09:21:10 -0700

    added s3 credentials without access keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar,
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials},
+                    styles: { medium: "300x300>", thumb: "100x100>" },
+                    default_url: "/images/:style/missing.png"
+
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  def s3_credentials
+    {:bucket => "xxx", :access_key_id => "xxx", :secret_access_key => "xxx"}
+  end
+
 
   has_many :posts
   has_many :comments
+
+
 end
