7b227ab796d3b5126b5cb781bd78b28d7e3a92a5
Michael Ray <raymichael23@gmail.com>
2016-06-08 14:44:20 -0700

    dropdown menu functional for viewing, editing, and logging out of profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
                     url: ":s3_domain_url",
                     path: "/:class/:attachment/:id_partition/:style/:filename",
                     styles: { medium: "300x300>", thumb: "100x100>" },
-                    default_url: "/images/:style/missing.png"
+                    default_url: "missing.png"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
